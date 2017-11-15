5fca9e4433455b333adc9b6150d065e3af9637c8
Dhaval Thakkar <dhaval93thakkar@gmail.com>
2017-11-14 17:14:14 -0800

    Dhaval

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <title>Dhaval  Learning Github</title>
+        <title>Learning Github</title>
 
 
         <!-- Fonts -->
