527d494c7ca6855926e38f69fa8d21446f62b57c
Dhaval Thakkar <prajesh.d9ithub@gmail.com>
2017-11-14 16:26:08 -0800

    Dhaval

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Prajesh Learning Github</title>
+        <title>Dhaval's first experience of Learning Github</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
