0323e0e8ea24f64ccec04504de908dcbac90fb5f
Kandarp Pandya <prajesh.d9ithub@gmail.com>
2017-11-14 16:23:56 -0800

    prajesh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Prajesh Learning Github</title>
+        <title>Prajesh's First experience to Learn Github</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
